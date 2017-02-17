2c940cf0e078ac2079b47a75796da311ed6acc96
yktangint <tang.yingkai@intellif.com>
2017-02-17 09:26:32 +0800

    add utilities

---
+++ src/main/java/com/weiwei/utils/ApplicationResource.java
@@ -0,0 +1,9 @@
+package com.weiwei.utils;
+
+import java.util.concurrent.ForkJoinPool;
+
+public class ApplicationResource {
+
+	public static final ForkJoinPool THREAD_POOL = new ForkJoinPool(Runtime.getRuntime().availableProcessors() + 4);
+	
+}
---
+++ src/main/java/com/weiwei/utils/FunctionUtils.java
@@ -0,0 +1,18 @@
+package com.weiwei.utils;
+
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class FunctionUtils {
+
+	public static Object waitTillThreadFinish(Future<?> thread) {
+		try {
+			return thread.get();
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
+		return Optional.ofNullable(null);
+	}
+	
+}
