95451659bdc6cc3363f9ca5e1a88c414a80d6630
yktangint <tang.yingkai@intellif.com>
2017-03-02 19:50:38 +0800

    config resouce server

--- src/main/java/com/weiwei/patent/security/config/ResourceServerConfig.java
+++ src/main/java/com/weiwei/patent/security/config/ResourceServerConfig.java
@@ -1,13 +1,30 @@
 package com.weiwei.patent.security.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 import com.weiwei.patent.common.Constants;
 
 @Configuration
+@EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Bean
+	public JdbcTokenStore tokenStore() {
+		return new JdbcTokenStore(dataSource);
+	}
+	
 	@Override
 	public void configure(final HttpSecurity http) throws Exception {
 		http
@@ -17,4 +34,10 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 			.authorizeRequests()
 			.antMatchers("/manage/**").authenticated();
 	}
+	
+	@Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.tokenStore(tokenStore());
+    }
+	
 }
